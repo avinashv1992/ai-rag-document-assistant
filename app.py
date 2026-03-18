947cc7a8ebdc427ab0437d17bfe56c2bcf196c3d
avinashv1992 <vibhute.avinash2@gmail.com>
2026-03-18 15:49:50 +0530

    Modification3 minor app.py:

--- app.py
+++ app.py
@@ -1,48 +1,57 @@
-import streamlit as st
+from fastapi import FastAPI
+from pydantic import BaseModel
+
 from langchain_community.vectorstores import Chroma
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_community.llms import Ollama
+from sentence_transformers import CrossEncoder
 
-# Title
-st.title("📄 AI Document Assistant")
+app = FastAPI()
 
-# Load embedding model
+# Load models (only once)
 embedding_model = HuggingFaceEmbeddings(
     model_name="sentence-transformers/all-MiniLM-L6-v2"
 )
 
-# Load vector database
 vector_db = Chroma(
     persist_directory="vector_db",
     embedding_function=embedding_model
 )
 
-retriever = vector_db.as_retriever(search_kwargs={"k":10})
+retriever = vector_db.as_retriever(search_kwargs={"k": 20})
+
+reranker = CrossEncoder("cross-encoder/ms-marco-MiniLM-L-6-v2")
 
 llm = Ollama(model="gemma:2b")
 
-# User input
-question = st.text_input("Ask a question from the document:")
 
-if question:
+class QueryRequest(BaseModel):
+    question: str
 
-    docs = retriever.invoke(question)
 
-    context = "\n".join([doc.page_content for doc in docs])
+@app.post("/query")
+def query_rag(req: QueryRequest):
+
+    docs = retriever.invoke(req.question)
+
+    pairs = [(req.question, doc.page_content) for doc in docs]
+    scores = reranker.predict(pairs)
+
+    scored_docs = sorted(zip(scores, docs), key=lambda x: x[0], reverse=True)
+    top_docs = [doc for _, doc in scored_docs[:8]]
+
+    context = "\n".join([doc.page_content for doc in top_docs])
 
     prompt = f"""
-    Answer the question using only the provided context.
+Answer only from context.
 
-    Context:
-    {context}
+Context:
+{context}
 
-    Question:
-    {question}
-
-    Answer:
-    """
+Question:
+{req.question}
+"""
 
     response = llm.invoke(prompt)
 
-    st.subheader("Answer")
-    st.write(response)
+    return {"answer": response}
